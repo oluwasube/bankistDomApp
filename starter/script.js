fb747cdf4fe01582682bb1646e2f0ccaed19af22
oluwasube <enibeke20@gmail.com>
2021-11-01 15:14:38 +0100

    used interesection observer Api for nav for optmimized performance

--- starter/script.js
+++ starter/script.js
@@ -9,6 +9,12 @@ const btnCloseModal = document.querySelector('.btn--close-modal');
 const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
 const btnScrollTo = document.querySelector('.btn--scroll-to');
 const section1 = document.querySelector('#section--1');
+const nav = document.querySelector('.nav');
+
+// Tabbed component
+const tabs = document.querySelectorAll('.operations__tab');
+const tabsContainer = document.querySelector('.operations__tab-container');
+const tabsContent = document.querySelectorAll('.operations__content');
 
 const openModal = function (e) {
   e.preventDefault();
@@ -75,27 +81,70 @@ document.querySelector('.nav__links').addEventListener('click', function (e) {
   }
 });
 
-// Tabbed component
-const tabs = document.querySelectorAll('.operations__tab');
-const tabsContainer = document.querySelector('.operations__tab-container');
-const tabsContent = document.querySelectorAll('.operations__content');
-
+// Tabbed components
 tabsContainer.addEventListener('click', function (e) {
   const clicked = e.target.closest('.operations__tab');
 
   if (!clicked) return;
 
   // remove active classes
-  tabs.forEach(t => t.classList.remove('.operations__tab--active'));
-  tabsContent.forEach(c => c.classList.remove('.operations__content--active'));
+  tabs.forEach(t => t.classList.remove('operations__tab--active'));
+  tabsContent.forEach(c => c.classList.remove('operations__content--active'));
 
   // Add active classes
 
-  clicked.classList.add('.operations__tab--active');
+  clicked.classList.add('operations__tab--active');
 
   // Activate content
 
   document
     .querySelector(`.operations__content--${clicked.dataset.tab}`)
-    .classList.add('.operations__content--active');
+    .classList.add('operations__content--active');
 });
+
+// Menu fade animation
+
+const handleHover = function (e, opacity) {
+  if (e.target.classList.contains('nav__link')) {
+    const linked = e.target;
+    const siblings = linked.closest('.nav').querySelectorAll('.nav__link');
+    const logo = linked.closest('.nav').querySelector('img');
+
+    siblings.forEach(el => {
+      if (el !== linked) el.style.opacity = opacity;
+    });
+    logo.style.opacity = opacity;
+  }
+};
+
+nav.addEventListener('mouseover', function (e) {
+  handleHover(e, 0.5);
+});
+nav.addEventListener('mouseout', function (e) {
+  handleHover(e, 1);
+});
+
+// Sticky navigation
+/*
+const intialCoords = section1.getBoundingClientRect();
+window.addEventListener('scroll', function (e) {
+  if (window.scrollY > intialCoords.top) nav.classList.add('sticky');
+  else nav.classList.remove('sticky');
+});
+*/
+
+// Sticky navigation using the observer intersection API
+const header = document.querySelector('.header');
+const navHeight = nav.getBoundingClientRect().height;
+const stickynav = function (entries) {
+  const [entry] = entries;
+  if (!entry.isIntersecting) nav.classList.add('sticky');
+  else nav.classList.remove('sticky');
+};
+
+const headerObserver = new IntersectionObserver(stickynav, {
+  root: null,
+  threshold: 0,
+  rootMargin: `-${navHeight}px`,
+});
+headerObserver.observe(header);
